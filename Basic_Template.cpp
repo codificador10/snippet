c3034cfe152886d333c382c14eed5e1e32de4a70
Ayush Raj <iamayushraj10@gmail.com>
2022-02-04 04:17:12 +0530

    v1

---
+++ Basic_Template.cpp
@@ -0,0 +1,59 @@
+/* codificador10 /- Ayush Raj(ऋ) */
+
+#pragma region
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef crazybarcafan
+#include "/home/codificador/debug.hpp"
+#else
+#define debug(...)
+#endif
+#define int			 int64_t // Undefine in case of tight constrains or memory limit exceeded
+#define lld			 long double
+#define nl			 '\n'
+#define vii			 vector<int>
+#define vvii			 vector<vector<int>>
+#define pii			 pair<int, int>
+#define mii			 map<int, int>
+#define ss			 second
+#define ff			 first
+#define pb			 push_back
+#define yes			 cout << "YES\n"
+#define no			 cout << "NO\n"
+#define sz(x)			 ((int)(x).size())
+#define all(x)			 (x).begin(), (x).end()
+#define modulo(x,y) 		 (x%y)<(int)0 ? ((x%y)+y) : (x%y);
+#define rep(i, a, b)		 for (int i = a; i < b; i++)
+#define rrep(i, a, b)		 for (int i = b - 1; i >= a; i--)
+#define precision		 cout << setprecision(10) << fixed
+#define FastIO			 ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+//operator overload
+template<typename T1>istream& operator>>(istream& in,vector<T1>&a){for(auto &it:a){in>>it;};return in;}
+template<typename T1>ostream& operator<<(ostream& out,vector<T1>&a){for(auto &it:a){out<<it<<" ";}return out;}
+const int mod = 1e9 + 7,MOD = 998244353,inf = 1e9;
+const int64_t INF = 1e18;
+// clang-format on
+#pragma endregion
+/*--------------------------------------------------solve----------------------------------------------------*/
+const int32_t MX = 1e5 + 5;
+
+void solve(int testo)
+{
+}
+
+/*--------------------------------------------------testcase--------------------------------------------------*/
+
+signed main()
+{
+    FastIO;
+    int32_t testcase = 1;
+    // cin>>testcase;
+    for (int32_t tt = 1; tt <= testcase; tt++)
+    {
+        solve(tt);
+    }
+    return 0;
+}
